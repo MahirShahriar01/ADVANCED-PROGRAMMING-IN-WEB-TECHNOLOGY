66ae86e76b835f4e9eb25fb5032840b7adcb8597
unknown <mahirshahriar786@gmail.com>
2022-12-18 01:50:03 +0600

    maail validaation

---
+++ Mail Validation/resources/views/sendmail.blade.php
@@ -0,0 +1,6 @@
+<p>Hi, This is {{ $data['name'] }}</p><br><br>
+<p>I have some query like, {{ $data['message'] }}.</p>
+<p>t would be really grateful if you could review this information.</p>
+<br><br>
+<p>Regards,</p>
+<p> this is my email {{ $data['email']}}</p>
